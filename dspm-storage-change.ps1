cbbbd84590cc62b1e83f504b552d07fc3a0f66b4
Erick Moore <erick.moore@gmail.com>
2024-12-11 15:08:11 -0500

    up

--- dspm-storage-change.ps1
+++ dspm-storage-change.ps1
@@ -1,62 +1,22 @@
 <#
 .SYNOPSIS
-    This script allows for the re-ip or re-creation of DIG | Prisma Cloud DSPM VNets used for data scanning.
+    This script modifies Azure Storage accounts in the Dig | DSPM hub scanning account to restrict access to the specified IP addresses.
 
 .DESCRIPTION
-    This script will look for all VNets in a resource group matching: "dig-security-rg-" and then find all VNets matching the tag "dig-security"
-    Once identified the script will remove all subnets and CIDR's, replace with specified CIDR.
+    This script will look for all storage accounts and VNets in a resource group matching: "dig-security-rg-"
+    Once identified the script will set the storage account firewall to deny access except to the regional VNet from Dig | DSPM and Dig | DSPM SaaS IP addresses.
 
-.PARAMETER Backup
-    Switch to backup existing VNet as JSON file.
-
-.PARAMETER Cidr
-    Address prefix to use for new VNets: 10.1.0.0/24
-
-.PARAMETER CreateVNet
-    Create/re-create new VNets based on defined regions.
-
-.PARAMETER Force
-    If CreateVNet is used, this will overwrite existing VNets instead of prompting to replace.
-
-.PARAMETER ImportFile
-    Import CSV file with CIDR's to use for each region. The CSV format should have a header row with the following columns:
-    Region, Cidr
-
-.PARAMETER Prompt
-    Switch to prompt user for new CIDR.
-
-.PARAMETER Regions
-    Comma-separated list of Azure regions: "westus,eastus,centralus"
-    When selected only the specified regions will be modified.
+.PARAMETER Ips
+    Specify Ip's to add to storage account firewall: "54.225.205.121, 18.214.146.232, 3.93.120.3"
     
 .EXAMPLE
-    Create VNet's in westus, eastus, and eastus2 regions.
+    Modify firewall to allow specified IP's
 
-    .\dspm-vnet-change.ps1 -CreateVNet -Regions "westus,eastus, eastus2" -Cidr 10.10.0.0/24
-
-.EXAMPLE
-    Modify existing VNets with new CIDR by importing a CSV file.
-
-    .\dspm-vnet-change.ps1 -ImportFile .\cidr.csv
-
-.EXAMPLE
-    Re-IP existing VNets with new CIDR.
-
-    .\dspm-vnet-change.ps1 -Cidr 10.10.0.0/24
-
-.EXAMPLE
-    Re-IP existing VNets with new CIDR and backup existing VNets.
-
-    .\dspm-vnet-change.ps1 -Cidr 10.10.0.0/24 -Backup
-
-.EXAMPLE
-    Re-IP existing VNets with new CIDR and prompt for each region CIDR.
-
-    .\dspm-vnet-change.ps1 -Cidr 10.10.0.0/24 -Prompt
+    .\dspm-storage-change.ps1 -Ips "54.225.205.121, 18.214.146.232, 3.93.120.3"
 
 .NOTES
     Author: Erick Moore
-    Date: 2024-12-09
+    Date: 2024-12-11
 
 #>
 
@@ -64,9 +24,6 @@
 param (
     [Parameter(Mandatory=$false, HelpMessage = "Allowed public IPs to allow. Default: '54.225.205.121, 18.214.146.232, 3.93.120.3'")]
     [string]$Ips
-    # [Parameter(Mandatory=$false, HelpMessage="Comma-separated list of Azure regions used for CreateVNet switch (e.g., 'westus,eastus,centralus')")]
-    # [ValidateSet("Logging, Metrics, AzureServices", "Logging, Metrics", "Logging, AzureServices", "Metrics, AzureServices", "Logging", "Metrics", "AzureServices", "")]
-    # [string]$Access
 )
 
 $tagName            = 'dig-security'
@@ -83,10 +40,9 @@ if ($Ips) { $publicIPs = $Ips.Split(",").Trim() }
 
 # Set Default values if not provided
 if (-not $Ips) { $publicIPs = $dspmIps }
-if (-not $Access) { $Access = $azureAccess }
 
 # ╔══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗
-# ║ Main Process: Loop through all discovered VNet's, find matching tag, remove all subnets and CIDR's, replace with specified CIDR.         ║
+# ║ Main Process: Loop through all discovered storage accounts in Dig | DSPM resource group, add VNet and required IP's to storage firewall. ║
 # ╚══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝
 foreach  ($storageAccount in $allStorageAccounts) {
 
@@ -97,7 +53,7 @@ foreach  ($storageAccount in $allStorageAccounts) {
     $percentComplete = ($counter / $storageCount) * 100
 
     # Progress bar
-    Write-Progress -Activity "Processing Storage Account $($storageAccount.StorageAccountName)" -Status "Processing Storage Account $counter of $storageCount" -PercentComplete $percentComplete -Id 1
+    Write-Progress -Activity "Storage Account: $($storageAccount.StorageAccountName) Region: $($storageAccount.Location)" -Status "Processing Storage Account $counter of $storageCount" -PercentComplete $percentComplete -Id 1
 
     # Skip if no matching VNet
     if (-not $currentVNet) { continue }
